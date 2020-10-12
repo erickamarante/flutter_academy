3cf96c4ee6af64097c93980c0adfc6aa8e1b2033
Erick Amarante <erick.amarante@truework.com.br>
2020-10-12 15:09:20 -0300

    chore: remove unused imports

--- modulo_5/desafio/desafio.dart
+++ modulo_5/desafio/desafio.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 // Baseado no array acima monte um relatório onde mostre
 // Apresente a quantidade de pacientes com mais de 20 anos
 // Agrupar os pacientes por familia(considerar o sobrenome) apresentar por familia.
